8aa8e3642c553d69cd407493cb46dec9d6aecc83
mrb <michaelbarrett@bluelily.com.au>
2025-07-18 13:16:13 +1000

    deleting circular files

--- curam-ai-python-v2/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services package for Australian Property Intelligence
-"""
-
-from .llm_service import LLMService
-from .property_service import PropertyAnalysisService
-from .stability_service import StabilityService
-
-__all__ = ['LLMService', 'PropertyAnalysisService', 'StabilityService']
